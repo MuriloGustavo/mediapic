468e97e824aeb2296b9aa2f097d13fb93ef3611c
MuriloGustavo <murilogustavo98@gmail.com>
2019-08-24 21:44:15 -0300

    adição do pipeFilter, photoResolver, photosComponent e subject com pipeDebounceTime

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
-import { PhotoService } from '../photo.service';
 import { Photo } from '../photo.model';
 
 @Component({
@@ -9,19 +10,23 @@ import { Photo } from '../photo.model';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
+  filter = '';
+  debounce: Subject<string> = new Subject<string>();
 
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-    ) { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    const userName = this.activatedRoute.snapshot.params.userName;
-    this.photoService.listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
+    this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy() {
+    this.debounce.unsubscribe();
   }
 
 }
---
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
+
+import { Photo } from '../../photo.model';
+
+@Component({
+  selector: 'app-photos',
+  templateUrl: './photos.component.html',
+  styleUrls: ['./photos.component.css']
+})
+export class PhotosComponent implements OnInit, OnChanges {
+
+  @Input() photos: Photo[] = [];
+  rows: any[] = [];
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.photos) {
+      this.rows = this.groupColumns(this.photos);
+    }
+  }
+
+  groupColumns(photos: Photo[]) {
+    const newRows = [];
+    for (let index = 0; index < photos.length; index += 3) {
+      newRows.push(photos.slice(index, index + 3));
+    }
+    return newRows;
+  }
+
+}
